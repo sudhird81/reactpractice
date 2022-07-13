50a0d956fd1b3fa33c3e2c5ccdb1409337cc060e
ebsprincekahol <109081269+ebsprincekahol@users.noreply.github.com>
2022-07-13 17:18:23 +0530

    SignUp1

--- basicreact/src/components/SignUp1.js
+++ basicreact/src/components/SignUp1.js
@@ -1,15 +1,45 @@
-
-import React from "react";
-
-class SignUp1 extends React.Component{
-    render(){
-        return(
-            <form>
-                
-            </form>
-        )
-    }
+import React, { useState } from "react";
+const SignUp1 =()=>{
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+    const [allEntry,setAllEntry]=useState([]);
+    const submitForm =(e)=>{ 
+        e.preventDefault();
+        const newEntry={email:email,password:password};
+        setAllEntry([...allEntry,newEntry]); 
+        console.log(allEntry)
+    } 
+    return(
+       <>
+        <form action="" onSubmit={submitForm}>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" autoComplete="off" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" autoComplete="off" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            </div>
+           <button type="submit">Login</button>
+        </form>
+        <div>
+            {
+                allEntry.map((curElem)=>{
+                    return(<div>
+                    <p>Email is: {curElem.email}</p>
+                    <p>Password is: {curElem.password}</p>
+                    
+                    </div>)
+                })
+            }
+        </div>
+       </>
+    )
 }
- 
-
 export default SignUp1;
+
+
+
+
+   
+
