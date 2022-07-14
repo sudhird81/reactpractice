d10b07d559a3a19d794a18d7d66ce7c1e03069ac
ebsdavindersingh <109080997+ebsdavindersingh@users.noreply.github.com>
2022-07-14 18:58:18 +0530

    I made a db,api

---
+++ BasicNode/router.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const db  = require('./db');
+const { signupValidation, loginValidation } = require('./validation');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+ 
+ 
+router.post('/login', loginValidation, (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE email = ${db.escape(req.body.email)};`,
+    (err, result) => { 
+      // user does not exists
+      if (err) {
+        throw err;
+        return res.status(400).send({
+          msg: err
+        });
+      }
+      if (!result.length) {
+        return res.status(401).send({
+          msg: 'Email or password is incorrect!'
+        });
+      }
+      // check password
+      bcrypt.compare(
+        req.body.password,
+        result[0]['password'],
+        (bErr, bResult) => {
+          // wrong password
+          if (bErr) {
+            throw bErr;
+            return res.status(401).send({
+              msg: 'Email or password is incorrect!'
+            });
+          }
+          if (bResult) {
+            const token = jwt.sign({id:result[0].id},'the-super-strong-secrect',{ expiresIn: '1h' });
+            db.query(
+              `UPDATE users SET last_login = now() WHERE id = '${result[0].id}'`
+            );
+            return res.status(200).send({
+              msg: 'Logged in!',
+              token,
+              user: result[0]
+            });
+          }
+          return res.status(401).send({
+            msg: 'Username or password is incorrect!'
+          });
+        }
+      );
+    }
+  );
+});
+ 
+module.exports = router;
