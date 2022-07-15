271a364bce471a1b22baecb99ddfc53dc71d344d
ebsprincekahol <109081269+ebsprincekahol@users.noreply.github.com>
2022-07-15 14:02:38 +0530

    Create Registration form

--- basicreact/src/components/SignUp.js
+++ basicreact/src/components/SignUp.js
@@ -1,80 +1,119 @@
-import { Button, Checkbox, Form, Input } from "antd";
 import React from "react";
+import { Form, Input, DatePicker, Checkbox, Button, Row, Col, Select } from "antd"
 const SignUp = () => {
-    const onFinish = (values) => {
-        console.log('Success:', values);
-    };
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <>
 
-    return (
-        <Form
-            name="basic"
+
+      <Row>
+        <Col span={12} offset={6}>
+          <Form name="basic"
             labelCol={{
-                span: 4,
+              span: 10,
             }}
             wrapperCol={{
-                span: 8,
+              span: 14,
             }}
             initialValues={{
-                remember: true,
+              remember: true,
             }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ]}
-            >
-                <Input />
+            autoComplete="off">
+
+            <Form.Item name="fullName" label="Full Name" rules={[{ required: true, 
+            message:"Please enter your name"},
+            {whitespace:true},
+            {min:3}, 
+            ]} hasFeedback>
+           
+              <Input placeholder="Type your name" />
             </Form.Item>
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-            >
-                <Input.Password />
+            <Form.Item name="email" label="Email"rules={[{ required: true, 
+            message:"Please enter your email"},
+            {type:'email',message:"Please enter a valid email" },
+             
+            ]} hasFeedback>
+              <Input placeholder="Type your email" />
             </Form.Item>
 
-            <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{
-                    offset: 4,
-                    span: 4,
-                }}
-            >
-                <Checkbox>Remember me</Checkbox>
+            <Form.Item name="password" label="Password"
+            
+            rules={[{ required: true, 
+            },   
+            {min:6},
+              
+            ]} hasFeedback>
+              <Input.Password placeholder="Type your password" />
             </Form.Item>
 
-            <Form.Item
-                wrapperCol={{
-                    offset: 4,
-                    span: 4,
-                }}
-            >
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
+            <Form.Item name="confirmPassword" label="Confirm Password" dependencies={['password']}
+            rules={[{ required: true, 
+            },
+            ({getFieldValue})=>({
+               validator(_,value){
+                if(!value || getFieldValue('password') === value) {
+                  return Promise.resolve()
+                }
+                return Promise.reject('The two passwords that you entered does not match.')
+               }
+            })
+             
+            ]} hasFeedback>
+              <Input.Password placeholder="Confirm  your password" />
             </Form.Item>
-        </Form>
-    );
-};
 
 
+            <Form.Item name="gender" label="Gender" requiredMark="optional">
+              <Select placeholder="Select your gender" >
+                <Select.Option value="male">Male</Select.Option>
+                <Select.Option value="female">Female</Select.Option>
+              </Select>
+            </Form.Item>
+
+
+            <Form.Item name="dob" label="Date of Birth" rules={
+              [{
+                 required:true, 
+                 message:'Please provide your date of birth',      
+              },
+ 
+              ]
+            }hasFeedback>
+              <DatePicker style={{ width: "100%" }} picker="date " placeholder="choose date of birth" />
+            </Form.Item>
+            {/* <Row>
+            <col span={10} offset={8}></col>
+           </Row>
+            <Form.Item name="agreement">
+                <Checkbox>Agree to ourTerm and Condition</Checkbox>
+            </Form.Item>
+            <Row></Row> */}
+
+            <Row>
+              <Col span={8} offset={5}></Col>
+              <Form.Item >
+                <Button style={{ width: "500%" }} type="primary" htmlType="submit">Register</Button>
+              </Form.Item>
+            </Row>
+
+
+
+
+
+          </Form>
+        </Col>
+      </Row>
+
+    </>
+  )
+}
 export default SignUp;
--- basicreact/src/components/SignUp1.js
+++ basicreact/src/components/SignUp1.js
@@ -1,39 +1,167 @@
 import React, { useState } from "react";
-const SignUp1 =()=>{
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
-    const [allEntry,setAllEntry]=useState([]);
-    const submitForm =(e)=>{ 
+import { Form, Col, Row, Input } from 'antd';
+import Test from "./Test";
+const SignUp1 = () => {
+    const [name, setName] = useState("");
+    // const [errorName, setErrorName] = useState("")
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+    // const [allEntry,setAllEntry]=useState([]);
+    const [showData, setshowData] = useState(false)
+
+    const enterName = (e) => {
+        setName(e.target.value)
+    }
+
+    const enterEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const enterPhone = (e) => {
+        setPhone(e.target.value)
+
+    }
+
+    const enterPassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+
+
+
+
+    const submitForm = (e) => {
         e.preventDefault();
-        const newEntry={email:email,password:password};
-        setAllEntry([...allEntry,newEntry]); 
-        console.log(allEntry)
-    } 
-    return(
-       <>
-        <form action="" onSubmit={submitForm}>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" autoComplete="off" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" autoComplete="off" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-            </div>
-           <button type="submit">Login</button>
-        </form>
-        <div>
+       
+
+        setshowData(true)
+
+
+        // const newEntry={email:email,password:password,fullname:name,phoneno:phone};
+        // setAllEntry([newEntry]); 
+        // setName("");
+        // setEmail("");
+        // setPassword("");
+        // setPhone("");
+    }
+    return (
+        <>
+
+            <Row gutter={[8, 8]}>
+                <Col span={8} />
+                <Col span={8}>
+
+                    <Form action="" className="inputstyle">
+                    <Form.Item 
+                            label="Full Name"
+                            name="fullname"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Your Name cannot be empty!',
+                                },
+                            ]}
+                        >
+                            <Input  type="text" placeholder="Enter your Name" name="fullname" id="fullname" autoComplete="off" value={name} onChange={enterName} />
+                        </Form.Item>
+
+                        
+
+
+                        <Form.Item 
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Your phone number cannot be empty!',
+                                },
+                            ]}
+                        >
+                            <Input type="text" placeholder="Enter your Email" name="email" id="email" autoComplete="off" value={email} onChange={enterEmail} />
+                        </Form.Item>
+                            
+                        
+
+
+                        <Form.Item 
+                            label="Phone Number"
+                            name="phoneno"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Your phone number cannot be empty!',
+                                },
+                            ]}
+                        >
+                            <Input type="number" placeholder="Enter your phone number" name="phoneno" id="phoneno" autoComplete="off" value={phone} onChange={enterPhone} />
+                        </Form.Item>
+
+
+
+
+                        <Form.Item 
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Your password cannot be empty!',
+                                },
+                            ]}
+                        >
+                            <Input type="password" placeholder="Enter your password" name="password" id="password" autoComplete="off" value={password} onChange={enterPassword} />
+
+                        </Form.Item>
+
+
+
+
+
+
+
+                        <Form.Item>
+                            <button onClick={submitForm}>Sign Up</button>
+
+                        </Form.Item>
+                       
+                    </Form>
+                    {showData ? <><p>Full name : {name}</p>
+                            <p>Email : {email}</p>
+                            <p>Phone : {phone}</p>
+                            <p>Password :{password}</p>
+                        </> : ""}
+                </Col>
+                <Col span={8} />
+            </Row>
+
+
+            {/* <div>
             {
                 allEntry.map((curElem)=>{
                     return(<div>
-                    <p>Email is: {curElem.email}</p>
-                    <p>Password is: {curElem.password}</p>
+                        <p> Full Name : {curElem.fullname}</p>
+                    <p>Email : {curElem.email}</p>
+                    <p>Phone Number : {curElem.phoneno}</p>
+                    <p>Password : {curElem.password}</p>
+                    
+                    
                     
                     </div>)
                 })
             }
-        </div>
-       </>
+        </div> */}
+
+        <Row>
+      <Col span={12} offset={6}>
+      {/* <Test /> */}
+      </Col>
+    </Row>
+
+
+
+        </>
     )
 }
 export default SignUp1;
@@ -41,5 +169,5 @@ export default SignUp1;
 
 
 
-   
+
 
