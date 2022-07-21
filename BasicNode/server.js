c3fd971bbdc0d517bbad0b014c8fc3afcfd9ccf5
ebsdavindersingh <109080997+ebsdavindersingh@users.noreply.github.com>
2022-07-21 16:30:18 +0530

    register

--- BasicNode/server.js
+++ BasicNode/server.js
@@ -7,7 +7,7 @@ const indexRouter = require('./router.js');
 const db  = require('./db');
  
 const app = express();
-  
+const bcrypt = require('bcryptjs');  
 app.use(express.json());
  
 app.use(bodyParser.json());
@@ -89,19 +89,108 @@ db.query(
   });
 
 });
-app.put('/user/add/:id', (req, resp) => {
+app.put('/user/update/:id', (req, res) => {
   const data =[req.body.name,req.body.email,req.body.password,req.params.id];
-  db.query("UPDATE employee SET name = ?,email =?, password =? where id =? ", data,(error,result,fields) =>{
-    if (error) throw error;
-    resp.send(result)
+  db.query("UPDATE employee SET name = ?,email =?, password =? where id =? ", data,(err,result) =>{
+    if(err){
+      var response = {
+        errorcode : err.code,
+        message : 'Got Error'
+      };
+      return res.json(response);
+    }
+    console.log(err);
+    console.log(result);
+    if(result){
+      var response = {
+        success : 'success',
+        message : 'User Got Updated'
+      };
+      return res.json(response);
+    }
   });
-    console.log(data,"result")
 });
 
-app.delete('/user/add/:id', (req,resp) => {
-  db.query("DELETE FROM employee WHERE id =" +req.params.id,(error,results) => {
-     if (error) throw error;
-     resp.send(results)
+app.delete('/user/delete/:id', (req,res) => {
+  db.query("DELETE FROM employee WHERE id =" +req.params.id,(err,result) => {
+    if(err){
+      var response = {
+        errorcode : err.code,
+        message : 'Got Error'
+      };
+      return res.json(response);
+    }
+    console.log(err);
+    console.log(result);
+    if(result){
+      var response = {
+        success : 'success',
+        message : 'User Got Deleted'
+      };
+      return res.json(response);
+    }
   });
   
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  var condition = email ? { email: { [Op.iLike]: `%${email}%` } } : null;
+  models.users.findAll({ where: condition })
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(200).json({
+          code: 400,
+          status: "error",
+          message: "Email not exist",
+        });
+      }
+      bcrypt.compare("'" + req.body.password + "'", user[0].password, function (
+        err,
+        result
+      ) {
+        if (err) {
+          return res.status(200).json({
+            code: 401,
+            status: "error",
+            message: "Auth failed",
+          });
+        } else if (result === true) {
+          const token = jsonwt.sign(
+            {
+              user_id: user[0].id,
+              email: user[0].email,
+            },
+            "secret",
+            {
+              expiresIn: "8h",
+            }
+          );
+          let name = user[0].firstname +' '+user[0].lastname;
+          return res.status(200).json({
+            code: 200,
+            status: "success",
+            message: "Auth successful",
+            name:name,
+            email: user[0].email,
+            token: token,
+            user_id:user[0].id,
+          });
+        } else {
+          return res.status(200).json({
+            code: 401,
+            status: "error",
+            message: "Incorrect password",
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+            code: 500,
+            status: "error",
+            error: err,
+            message: "Internal error occured",
+      });
+    });
 });
