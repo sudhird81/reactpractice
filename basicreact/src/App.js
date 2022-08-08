d8adebe096a367db7428ca0a610232b1d54031f2
ebsprincekahol <109081269+ebsprincekahol@users.noreply.github.com>
2022-08-08 09:35:31 +0530

    about us

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -3,13 +3,22 @@ import "antd/dist/antd.css";
 import "./App.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import LayoutComponents from "./LayoutComponents";
+
+import Login from "./components/Login";
+
+
+// import LayoutPage from "./componentss/LayoutPage";
+// import LayoutComponents from "./LayoutComponents";
 
 function App() {
   return (
     <div className="App">
+    {/* <LayoutPage /> */}
+   
+    <Login />
+    
       <BrowserRouter>
-        <LayoutComponents />
+        {/* <LayoutComponents /> */}
       </BrowserRouter>
     </div>
   );
